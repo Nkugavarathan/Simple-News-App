00b1144da7272eb48eabb15db919cc43d03338d0
Nkugavarathan <kugavarathan28@gmail.com>
2025-02-07 15:00:45 +0530

    fixed card

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -12,7 +12,7 @@ const NewsItem = ({ title, description, src, url }) => {
         }}
       >
         <img
-          src={src}
+          src={src || ""}
           className="card-img-top"
           alt="news-thumbnail"
           style={{
@@ -23,12 +23,31 @@ const NewsItem = ({ title, description, src, url }) => {
         />
         <div className="card-body">
           <h5 className="card-title">{title.slice(0, 50)}</h5>
-          <p className="card-text">
-            {description ? description.slice(0, 90) : "News"}
-          </p>
-          <a href={url} className="btn btn-primary">
-            Read More
-          </a>
+          <div
+            style={{
+              position: "relative", // Relative positioning to control the "Read More" button
+              paddingBottom: "40px", // Space for the button at the bottom
+            }}
+          >
+            <p className="card-text">
+              {description ? description.slice(0, 90) : "News"}
+            </p>
+
+            {/* The "Read More" button is positioned absolutely relative to the description */}
+            <a
+              href={url}
+              className="btn btn-primary mb-3"
+              target="_blank"
+              style={{
+                position: "absolute", // Position the button absolutely within the wrapper
+                bottom: "-3px", // Stick the button to the bottom of the description
+                left: "50%", // Center horizontally
+                transform: "translateX(-50%)", // Fix centering offset
+              }}
+            >
+              Read More
+            </a>
+          </div>
         </div>
       </div>
     </>
